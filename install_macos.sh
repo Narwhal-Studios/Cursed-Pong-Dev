221935d584ac961bf5ce23a52c53692a41aa5d83
cursedpongdevs <cursedpong@gmail.com>
2023-07-19 18:19:29 -0700

    Updating the install_macos.sh script

--- install_macos.sh
+++ install_macos.sh
@@ -3,10 +3,10 @@ cd ~/Library/'Application Support'
 mkdir cursed_pong
 cd cursed_pong
 echo "Downloading application files..."
-wget https://github.com/cursedpongdevs/cursed_pong/files/files.zip
+wget https://cursedpongdevs.github.io/cursed_pong/files/files.zip
 unzip files.zip
 rm files.zip
 cd ~/Applications
 echo "Downloading application..."
-wget https://github.com/cursedpongdevs/cursed_pong/files/cursed_pong.app
+wget https://cursedpongdevs.github.io/cursed_pong/files/cursed_pong.app
 echo "Done!"
